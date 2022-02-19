5fc5a64d22bfd674634d50644e5cd772d789267b
amol pawar <amoljp19@gmail.com>
2022-02-19 15:55:19 +0530

    unit test cases for insert update and delete also passed for NoteRepositoryTest.kt

--- app/src/test/java/com/task/noteapp/data/repository/NoteRepositoryTest.kt
+++ app/src/test/java/com/task/noteapp/data/repository/NoteRepositoryTest.kt
@@ -4,19 +4,20 @@ import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.task.noteapp.CoroutineTestRule
 import com.task.noteapp.MockNoteUtils
 import com.task.noteapp.data.local.dao.NoteDao
+import com.task.noteapp.data.local.model.Note
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runBlockingTest
-import org.junit.Assert
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.mockito.ArgumentCaptor
 import org.mockito.Mock
-import org.mockito.Mockito
 import org.mockito.MockitoAnnotations
+import org.mockito.kotlin.argumentCaptor
 import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 
 class NoteRepositoryTest {
@@ -41,6 +42,52 @@ class NoteRepositoryTest {
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
+    fun insertNoteTest() {
+        coroutineTestRule.testDispatcher.runBlockingTest {
+            val mockNote = MockNoteUtils.getMockNote()
+            noteRepository.insertNote(mockNote)
+
+            val argumentCaptor = argumentCaptor<Note>()
+            verify(noteDao).insertNote(argumentCaptor.capture())
+            assertEquals(mockNote.title, argumentCaptor.firstValue.title);
+
+        }
+
+    }
+
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun updateNoteTest() {
+        coroutineTestRule.testDispatcher.runBlockingTest {
+            val mockNote = MockNoteUtils.getMockNote()
+            noteRepository.updateNote(mockNote.copy(tag = "updated tag"))
+
+            val argumentCaptor = argumentCaptor<Note>()
+            verify(noteDao).updateNote(argumentCaptor.capture())
+            assertEquals("updated tag", argumentCaptor.firstValue.tag);
+        }
+
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun deleteNoteTest() {
+        coroutineTestRule.testDispatcher.runBlockingTest {
+            val mockNote = MockNoteUtils.getMockNote()
+            noteRepository.deleteNote(mockNote)
+
+            val argumentCaptor = argumentCaptor<Note>()
+            verify(noteDao).deleteNote(argumentCaptor.capture())
+            assertEquals(mockNote.title, argumentCaptor.firstValue.title);
+
+        }
+
+    }
+
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
     fun getAllNotesTest() {
         coroutineTestRule.testDispatcher.runBlockingTest {
             val mockNotes = MockNoteUtils.getMockNotes()
@@ -51,9 +98,9 @@ class NoteRepositoryTest {
 
             val data = noteRepository.getAllNotes()
 
-            Assert.assertEquals(data.first().get(0), mockNotes.get(0))
-            Assert.assertEquals(data.first().get(1), mockNotes.get(1))
-            Assert.assertEquals(data.first().get(2), mockNotes.get(2))
+            assertEquals(data.first().get(0), mockNotes.get(0))
+            assertEquals(data.first().get(1), mockNotes.get(1))
+            assertEquals(data.first().get(2), mockNotes.get(2))
         }
     }
 }
