fe6f254110ada68b72fae299c59f8e6d7dc0bb5f
amol pawar <amoljp19@gmail.com>
2022-02-19 14:16:20 +0530

    getAllNotes test case also passed for repository unit testing

--- app/src/test/java/com/task/noteapp/data/repository/NoteRepositoryTest.kt
+++ app/src/test/java/com/task/noteapp/data/repository/NoteRepositoryTest.kt
@@ -5,8 +5,10 @@ import com.task.noteapp.CoroutineTestRule
 import com.task.noteapp.MockNoteUtils
 import com.task.noteapp.data.local.dao.NoteDao
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -14,6 +16,7 @@ import org.mockito.ArgumentCaptor
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.MockitoAnnotations
+import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.whenever
 
 class NoteRepositoryTest {
@@ -38,10 +41,19 @@ class NoteRepositoryTest {
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun getAllNotes() {
+    fun getAllNotesTest() {
         coroutineTestRule.testDispatcher.runBlockingTest {
-            val mockData = MockNoteUtils.getMockNote()
+            val mockNotes = MockNoteUtils.getMockNotes()
+
+            whenever(noteDao.getAllNotes()) doReturn flowOf(
+                mockNotes
+            )
+
+            val data = noteRepository.getAllNotes()
+
+            Assert.assertEquals(data.first().get(0), mockNotes.get(0))
+            Assert.assertEquals(data.first().get(1), mockNotes.get(1))
+            Assert.assertEquals(data.first().get(2), mockNotes.get(2))
         }
     }
-    
 }
