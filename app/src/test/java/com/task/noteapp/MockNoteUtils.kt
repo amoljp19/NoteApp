f5ccd4d5e6059ad8116c0a3e0206a45d6ea85137
amol pawar <amoljp19@gmail.com>
2022-02-19 14:07:47 +0530

    added mock notes method as well

--- app/src/test/java/com/task/noteapp/MockNoteUtils.kt
+++ app/src/test/java/com/task/noteapp/MockNoteUtils.kt
@@ -13,4 +13,30 @@ object MockNoteUtils {
             tag = "tag"
         )
     }
+
+    internal fun getMockNotes() : List<Note> {
+        return listOf(
+            Note(
+                id = 1,
+                title = "title1",
+                description = "description1",
+                date = 1L,
+                tag = "tag1"
+            ),
+            Note(
+                id = 2,
+                title = "title2",
+                description = "descriptio2",
+                date = 2L,
+                tag = "tag2"
+            ),
+            Note(
+                id = 3,
+                title = "title3",
+                description = "descriptio3",
+                date = 3L,
+                tag = "tag3"
+            )
+        )
+    }
 }
