03d70a179bdc394afede43aea477aa9122e08ba8
amol pawar <amoljp19@gmail.com>
2022-02-20 19:35:22 +0530

    added getNotes() and mutable live data for list of notes

--- app/src/main/java/com/task/noteapp/note/viewmodel/NoteViewModel.kt
+++ app/src/main/java/com/task/noteapp/note/viewmodel/NoteViewModel.kt
@@ -1,16 +1,29 @@
 package com.task.noteapp.note.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.task.noteapp.data.local.model.Note
 import com.task.noteapp.data.repository.NoteRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class NoteViewModel @Inject constructor(
     private val noteRepository: NoteRepository
 ) : ViewModel() {
+
+    private val _notesLiveData = MutableLiveData<List<Note>>()
+
+    val notesLiveData: LiveData<List<Note>> = _notesLiveData
+
+    init {
+        getNotes()
+    }
 
     suspend fun insertNote(note: Note) {
         noteRepository.insertNote(note)
@@ -26,6 +39,14 @@ class NoteViewModel @Inject constructor(
 
     fun getAllNotes(): Flow<List<Note>> {
         return noteRepository.getAllNotes()
+    }
+
+    fun getNotes() {
+        viewModelScope.launch {
+            getAllNotes().collect {
+                _notesLiveData.value = it
+            }
+        }
     }
 
 }
