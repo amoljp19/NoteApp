8141a06ede1dfbcd83a9cf8f61021dab01530bf7
amol pawar <amoljp19@gmail.com>
2022-02-20 10:59:25 +0530

    compose preview not working resolved that issue

--- app/src/main/java/com/task/noteapp/note/ui/components/NoteItem.kt
+++ app/src/main/java/com/task/noteapp/note/ui/components/NoteItem.kt
@@ -1,7 +1,17 @@
 package com.task.noteapp.note.ui.components
 
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun NoteItem() {
+    Text("Note Item")
+}
+
+
+@Preview(showSystemUi = true)
+@Composable
+fun NoteItemPreview() {
+    NoteItem()
 }
